8957acb92492e628f22d38eeb633d1d661b40415
Mohammad Nadeem <nadeem_1359@yahoo.co.in>
2018-04-29 17:12:10 +0530

    Crossover assessment

---
+++ crossblog.tests/Controllers/ArticlesControllerTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using crossblog.Controllers;
+using crossblog.Domain;
+using crossblog.Model;
+using crossblog.Repositories;
+using FizzWare.NBuilder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace crossblog.tests.Controllers
+{
+    public class ArticlesControllerTests
+    {
+        private ArticlesController _articlesController;
+
+        private Mock<IArticleRepository> _articleRepositoryMock = new Mock<IArticleRepository>();
+
+        public ArticlesControllerTests()
+        {
+            _articlesController = new ArticlesController(_articleRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task Search_ReturnsEmptyList()
+        {
+            // Arrange
+            var articleDbSetMock = Builder<Article>.CreateListOfSize(3).Build().ToAsyncDbSetMock();
+            _articleRepositoryMock.Setup(m => m.Query()).Returns(articleDbSetMock.Object);
+
+            // Act
+            var result = await _articlesController.Search("Invalid");
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as OkObjectResult;
+            Assert.NotNull(objectResult);
+
+            var content = objectResult.Value as ArticleListModel;
+            Assert.NotNull(content);
+
+            Assert.Equal(0, content.Articles.Count());
+        }
+
+        [Fact]
+        public async Task Search_ReturnsList()
+        {
+            // Arrange
+            var articleDbSetMock = Builder<Article>.CreateListOfSize(3).Build().ToAsyncDbSetMock();
+            _articleRepositoryMock.Setup(m => m.Query()).Returns(articleDbSetMock.Object);
+
+            // Act
+            var result = await _articlesController.Search("Title");
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as OkObjectResult;
+            Assert.NotNull(objectResult);
+
+            var content = objectResult.Value as ArticleListModel;
+            Assert.NotNull(content);
+
+            Assert.Equal(3, content.Articles.Count());
+        }
+
+        [Fact]
+        public async Task Get_NotFound()
+        {
+            // Arrange
+            _articleRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Article>(null));
+
+            // Act
+            var result = await _articlesController.Get(1);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as NotFoundResult;
+            Assert.NotNull(objectResult);
+        }
+
+        [Fact]
+        public async Task Get_ReturnsItem()
+        {
+            // Arrange
+            _articleRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Article>(Builder<Article>.CreateNew().Build()));
+
+            // Act
+            var result = await _articlesController.Get(1);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as OkObjectResult;
+            Assert.NotNull(objectResult);
+
+            var content = objectResult.Value as ArticleModel;
+            Assert.NotNull(content);
+
+            Assert.Equal("Title1", content.Title);
+        }
+
+        [Fact]
+        public async void PostSetsLocationHeader()
+        {
+            _articleRepositoryMock.Setup(x => x.)
+            // Act
+            IActionResult actionResult = _articlesController.Post(new ArticleModel { Id = 10, Title = "ArticleModel1" });
+            var createdResult = actionResult as CreatedAtRouteNegotiatedContentResult<Product>;
+
+            // Assert
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual("DefaultApi", createdResult.RouteName);
+            Assert.AreEqual(10, createdResult.RouteValues["id"]);
+        }
+    }
+}
---
+++ crossblog.tests/Controllers/CommentsControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using crossblog.Controllers;
+using crossblog.Domain;
+using crossblog.Model;
+using crossblog.Repositories;
+using FizzWare.NBuilder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace crossblog.tests.Controllers
+{
+    public class CommentsControllerTests
+    {
+        private CommentsController _commentsController;
+        private ArticlesController _articlesController;
+
+        private Mock<IArticleRepository> _articleRepositoryMock = new Mock<IArticleRepository>();
+        private Mock<ICommentRepository> _commentsRepositoryMock = new Mock<ICommentRepository>();
+
+        public CommentsControllerTests()
+        {
+            _commentsController = new CommentsController(_articleRepositoryMock.Object, _commentsRepositoryMock.Object);
+        }
+    }
+}
